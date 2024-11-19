10a66e48d519ea2c83e53dd1a3f4a0332565ee19
MatteoDiMario96 <dm.matteo96@live.it>
2024-11-19 20:26:56 +0100

    The image.

---
+++ src/assets/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main(){
+
+    return(
+        <>
+            <main className="imgContainer">
+                <img src="mars.png" alt="Picture of the red planet, the planet for the King Elon." />
+            </main>
+        </>
+    )
+}
