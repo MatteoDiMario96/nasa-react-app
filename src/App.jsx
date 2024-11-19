b610e7bf77c4ed10ab6849ef3400b6723568ac96
MatteoDiMario96 <dm.matteo96@live.it>
2024-11-19 20:25:39 +0100

    Put the components inside with some logic if showModal is true show the components SideBar. With the function handleToggleMode you can turn the State true or false and pass in the props for the Main and SideBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
+import Footer from "./assets/components/Footer";
+import Main from "./assets/components/Main";
+import SideBar from "./assets/components/SideBar";
 
 
 function App() {
 
+  const [showModal, setShowModal] = useState(false)
+
+  function handleToggleModal() {
+    setShowModal(!showModal)
+    console.log('Changed Modal')
+  }
+
   return (
     <>
-      
-    </>
+      <Main />
+      {showModal && (
+        <SideBar handleToggleModal={handleToggleModal} />
+      )}
+      <Footer  showModal={showModal} handleToggleModal={handleToggleModal} />
+    </>  
   )
 }
 
