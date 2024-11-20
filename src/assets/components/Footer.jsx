d41489d1429abf3cbff3a7921f1ef84397a94820
MatteoDiMario96 <dm.matteo96@live.it>
2024-11-20 20:47:25 +0100

    Added the calendar for choose the day Transform the title in upperCase

--- src/assets/components/Footer.jsx
+++ src/assets/components/Footer.jsx
@@ -1,18 +1,23 @@
 export default function Footer(props) {
 
-    const {handleToggleModal} = props;
+    const { handleToggleModal, handleToggleModalDate, data } = props;
     return (
         <>
-        <footer>
-            <div className="bgGradient"></div>
-            <div>
-                <h1>NASA APOD REACT PROJECT</h1>
-                <h2>The landing of the red planet, come to NASA for visit it. </h2>
-            </div>
-            <button onClick={handleToggleModal}>
-                <i className="fa-solid fa-circle-info"></i>
-            </button>
-        </footer>
+            <footer>
+                <div className="bgGradient"></div>
+                <div>
+                    <h1>NASA APOD REACT PROJECT</h1>
+                    <h2>{data.title.toUpperCase()}</h2>
+                </div>
+                <div className="footer-button">
+                <button onClick={handleToggleModal}>
+                    <i className="fa-solid fa-circle-info"></i>
+                </button>
+                <button onClick={handleToggleModalDate}>
+                    <i class="fa-solid fa-calendar-days"></i>
+                </button>
+                </div>
+            </footer>
         </>
     )
 }
