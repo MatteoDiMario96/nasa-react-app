e73f025688b1d028128727ef6d4337c8fa175c3b
MatteoDiMario96 <dm.matteo96@live.it>
2024-11-21 04:36:47 +0100

    Added the loading logic for the image

--- src/assets/components/Main.jsx
+++ src/assets/components/Main.jsx
@@ -1,10 +1,29 @@
-export default function Main(props){
+import { useEffect, useState } from "react";
 
-    const {data} = props;
-    return(
+export default function Main(props) {
+
+    const { data } = props;
+    const [isImageLoading, setIsImageLoading] = useState(true);
+
+    function handleImageLoad() {
+        setIsImageLoading(false);
+    }
+
+    useEffect(() => {
+        // Quando cambiano i dati, riattiva il loading per la nuova immagine
+        setIsImageLoading(true);
+    }, [data]);
+    return (
         <>
             <main className="imgContainer">
-                <img src={data.url} alt={data.title}/>
+                {isImageLoading && (
+                    <div className="loadingState">
+                        <i className="fa-solid fa-gear"></i>
+                    </div>
+                )
+                }
+                <img src={data.hdurl} alt={data.title} onLoad={handleImageLoad} // Quando l'immagine è caricata
+                    style={{ display: isImageLoading ? "none" : "block" }} />
             </main>
         </>
     )
